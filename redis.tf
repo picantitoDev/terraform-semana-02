f5024c84b5d5e7805bb847530eaa9bda067a9b4c
valentino <avrg2005@hotmail.com>
2025-09-03 01:07:35 -0500

    feat: Creado archivo de configuracion de redis

--- redis.tf
+++ redis.tf
@@ -6,13 +6,13 @@ resource "docker_container" "redis" {
   name = "redis"
   image = docker_image.redis.image_id
 
+  networks_advanced {
+    name = docker_network.persistence_net.name
+  }
+
   ports {
     internal = 6379
     external = 6379
-  }
-
-  networks_advanced {
-    name = docker_network.persistence_net.name
   }
 
   restart = "always"
