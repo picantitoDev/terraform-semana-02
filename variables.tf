8a4786f64a84271d9c3d65d7d4b651caa26d74f9
MariDavalos <lissetalfaro07@gmail.com>
2025-09-03 19:37:51 -0500

    merge: resolver conflicto en variables.tf

--- variables.tf
+++ variables.tf
@@ -3,5 +3,10 @@ variable "nginx_container_count" {
 }
 
 variable "nginx_base_port" {
+<<<<<<< HEAD
   type = number
 }
+=======
+ type = number
+}
+>>>>>>> 45007af (feat: actualizar nombre de contenedores para incluir workspace en nginx)
