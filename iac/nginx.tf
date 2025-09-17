7d4db2ce013c1bf2d4a4329b5ef5bed5d3db91a4
Piero Arturo <155034777+picantitoDev@users.noreply.github.com>
2025-09-17 09:16:02 -0500

    feat: agregar volumenes a cada webserver

--- iac/nginx.tf
+++ iac/nginx.tf
@@ -14,6 +14,12 @@ resource "docker_container" "nginx_1" {
   networks_advanced {
     name = docker_network.persistence_net.name
   }
+
+  volumes {
+    host_path      = abspath("${path.module}/../host_volumes/web1")
+    container_path = "/usr/share/nginx/html"
+    read_only      = false
+  }
 }
 
 resource "docker_container" "nginx_2" {  
@@ -27,6 +33,12 @@ resource "docker_container" "nginx_2" {
   networks_advanced {
     name = docker_network.persistence_net.name
   }
+
+  volumes {
+    host_path      = abspath("${path.module}/../host_volumes/web2")
+    container_path = "/usr/share/nginx/html"
+    read_only      = false
+  }
 }
 
 resource "docker_container" "nginx_3" {  
@@ -39,5 +51,11 @@ resource "docker_container" "nginx_3" {
   
   networks_advanced {
     name = docker_network.persistence_net.name
+  }
+
+  volumes {
+    host_path      = abspath("${path.module}/../host_volumes/web3")
+    container_path = "/usr/share/nginx/html"
+    read_only      = false
   }
 }
